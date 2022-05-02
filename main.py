e61ab61c937f3966abeb800a024beb97e89f0537
andrew-creegan-demo <andrew.creegan.demo@gmail.com>
2022-05-02 17:43:27 +1200

    put configuration at the top of the file

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import integrate, signal
 
+input_filename = "data.csv"
+filter_window_length = 20
+filter_order = 2
+dx = 0.01
+initial = 0
+
 # Read data
-flow = np.genfromtxt("data.csv")
+flow = np.genfromtxt(input_filename)
 
 # Calculate volume
-flow_filtered = signal.savgol_filter(flow, 20, 2)
-volume = integrate.cumulative_trapezoid(flow_filtered, dx=0.01, initial=0)
+flow_filtered = signal.savgol_filter(flow, filter_window_length, filter_order)
+volume = integrate.cumulative_trapezoid(flow_filtered, dx=dx, initial=initial)
 
 # Plot results
 plt.plot(flow, label="Flow")
