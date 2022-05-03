c27f6ef4f0099571a41ddcd0f23ba2362809c79b
andrew-creegan-demo <andrew.creegan.demo@gmail.com>
2022-05-03 13:24:04 +1200

    broke main.py into functions
    added a test for calculate_volume

--- main.py
+++ main.py
@@ -5,27 +5,43 @@ import json
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
-# Define settings
-config_filename = "conf.json"
-with open(config_filename, "r") as file:
-    conf = json.load(file)
 
-# Select data file
-Tk().withdraw()
-data_filename = askopenfilename(title="Select data file", initialdir=".")
-Tk().destroy()
+def main():
+    # Define settings
+    config_filename = "conf.json"
+    with open(config_filename, "r") as file:
+        conf = json.load(file)
 
-# Read data
-flow = np.genfromtxt(data_filename)
+    # Select data file
+    Tk().withdraw()
+    data_filename = askopenfilename(title="Select data file", initialdir=".")
+    Tk().destroy()
 
-# Calculate volume
-flow_filtered = signal.savgol_filter(flow, conf["filter_window_length"], conf["filter_order"])
-volume = integrate.cumulative_trapezoid(flow_filtered, dx=conf["dx"], initial=conf["initial"])
-max_volume = volume[np.argmax(volume)]
-print(f"max volume: {max_volume}")
+    # Read data
+    flow = np.genfromtxt(data_filename)
 
-# Plot results
-plt.plot(flow, label="Flow")
-plt.plot(volume, label="Volume")
-plt.legend()
-plt.show()
+    # Calculate volume
+    volume = calculate_volume(flow, conf["dx"], conf["initial"],
+                              conf["filter_window_length"], conf["filter_order"])
+    max_volume = volume[np.argmax(volume)]
+    print(f"max volume: {max_volume}")
+
+    # Plot results
+    plt.plot(flow, label="Flow")
+    plt.plot(volume, label="Volume")
+    plt.legend()
+    plt.show()
+
+
+def calculate_volume(flow, dx, initial, filter_window_length=None, filter_order=None):
+    if filter_window_length is not None and filter_order is not None:
+        flow_filtered = signal.savgol_filter(flow, filter_window_length, filter_order)
+    else:
+        flow_filtered = flow
+    volume = integrate.cumulative_trapezoid(flow_filtered, dx=dx, initial=initial)
+    return volume
+
+
+if __name__ == "__main__":
+    main()
+
